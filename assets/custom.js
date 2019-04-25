f0a38aeac51a2c90adba435ddd18ef1f95b30710
atheng <atheng@ucalgary.ca>
2019-04-25 14:50:55 -0600

    use accesstoken

--- assets/custom.js
+++ assets/custom.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
         userId: '224007036',
         limit: 12,
         resolution: 'standard_resolution',
-        clientId: '7e2efb0e47994ac9bd9a079a3fdb5146',
+        accessToken: '8272226300.1677ed0.ff10fbc86e39462d8e21cade298ab1b6',
         sortBy: 'most-recent',
         template: '<div class="col-lg-3 instaimg"><a href="{{image}}" title="{{caption}}" target="_blank"><img src="{{image}}" alt="{{caption}}" class="img-fluid"/></a></div>',
     });
